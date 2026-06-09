eded2e8c1c7463dc198df877a82952807525861b
syedayan-05 <syedayan4128@gmail.com>
2026-06-09 14:17:19 +0530

    Add global validation exception handling

--- e-commerce_backend/src/main/java/com/ayan/ecommerce/exception/GlobalExceptionHandler.java
+++ e-commerce_backend/src/main/java/com/ayan/ecommerce/exception/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package com.ayan.ecommerce.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -22,5 +24,22 @@ public class GlobalExceptionHandler {
                                 ex.getMessage()
                         )
                 );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>>
+    handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        ));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
